1f3988e79e8ae98ce8d8f2ce66ece0f7d6bc3d03
raad1masum <piraadwan@gmail.com>
2020-04-02 11:49:42 -0700

    add 1186A

---
+++ Codeforces/1186A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     int n, m, k; cin >> n >> m >> k;
+
+     if (n <= m && n <= k)
+     {
+          cout << "Yes" << endl;
+          return 0;
+     }
+     cout << "No" << endl;
+}
