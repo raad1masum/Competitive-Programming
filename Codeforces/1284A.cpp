9e715eac7d475c47f47a58b761fe42840fc06b7c
raad1masum <piraadwan@gmail.com>
2020-04-04 10:35:21 -0700

    add 1284

---
+++ Codeforces/1284A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     string s[22], t[22];
+     int n, m; cin >> n >> m;
+
+     for (int i=0; i<n; i++)
+     {
+          cin >> s[i];
+     }
+     for (int i=0; i<m; i++)
+     {
+          cin >> t[i];
+     }
+
+     int q; cin >> q;
+     while (q--)
+     {
+          int x; cin >> x; x--;
+          cout << s[x%n] + t[x%m] << endl;
+     }
+}
