cd0e90b3ea17b14c3546fd2a8162fece0bccdc0a
raad1masum <piraadwan@gmail.com>
2020-04-08 20:57:15 -0700

    add 1328A

---
+++ Codeforces/1328A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     int n, a, b, c; cin >> n;
+
+     while (n--)
+     {
+          cin >> a >> b;
+          if (a%b == 0) cout << 0 << endl;
+          else cout << b-a%b << endl;
+     }
+
+     return 0;
+}
