566a0eb25a2e038554cd40b1800e013e8aa6dc26
raad1masum <piraadwan@gmail.com>
2020-07-28 17:58:37 -0700

    update template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) a*a
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
 using namespace std;
-using ll = int64_t;
 
 int main() {
     ios_base::sync_with_stdio(false);
