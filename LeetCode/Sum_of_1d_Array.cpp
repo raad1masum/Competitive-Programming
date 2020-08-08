16c1be6f18ed3a8368deebb3c06439bdf4017c22
raad1masum <piraadwan@gmail.com>
2020-08-08 10:02:05 -0700

    add solution

--- LeetCode/Sum_of_1d_Array.cpp
+++ LeetCode/Sum_of_1d_Array.cpp
@@ -1,14 +1,14 @@
 class Solution {
-    public:
-        vector<int> runningSum(vector<int>& nums) {
-            vector<int> out;
-            for (int i=0; i<nums.size(); ++i) {
-                int cnt=0;
-                for (int j=i; j>=0; --j) {
-                    cnt+=nums[j];
-                }
-                out.push_back(cnt);
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> out;
+        for (int i=0; i<nums.size(); ++i) {
+            int cnt=0;
+            for (int j=i; j>=0; --j) {
+                cnt+=nums[j];
             }
-            return out;
+            out.push_back(cnt);
         }
+        return out;
+    }
 };
